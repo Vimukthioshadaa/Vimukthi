1a8a9583b7155ab1412148cb506f1b9ae68e073c
Vimukthioshadaa <88750419+Vimukthioshadaa@users.noreply.github.com>
2022-02-15 18:40:42 +0530

    mr.vimuwa

---
+++ shop/allpayment.js
@@ -0,0 +1,31 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY 𝖵𝗂𝗆𝗎𝗄𝗍𝗁𝗂 𝖮𝗌𝗁𝖺𝖽𝖺
+
+*さ BANK*
+1. BRI 
+	ㅁ 999999999
+		• R***A*DI
+		
+*さ E-MONEY*
+1. GOPAY
+	ㅁ 99999999
+		• ZE**** **C
+2. OVO
+	ㅁ 999999
+		• EL*****TI
+3. DANA
+	ㅁ 999999
+		• ZE**** **C
+
+*さ PULSA*
+1. SMARTFREN
+	ㅁ 99999999
+2. TELKOMSEL
+	ㅁ 9999
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
